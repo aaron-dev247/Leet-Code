6204c84824449409478f168e03d2c7eae62424e4
Aaron <troublemaker.5354@gmail>
2023-09-27 21:17:13 +0800

    Container With Most Water solution

--- Medium/ContainerWithMostWater.cpp
+++ Medium/ContainerWithMostWater.cpp
@@ -12,24 +12,23 @@ class Solution
 public:
     int maxArea(vector<int> &height)
     {
-        int max_area = 0, base = height.size() - 1;
+        int max_area = 0;
 
         // start from left most and right most indexes
-        int left = 0, right = base;
+        int left = 0, right = height.size() - 1;
         while (left < right)
         {
             // move the left or right index based on which side is the smallest
             if (height[left] < height[right])
             {
-                max_area = max(max_area, height[left] * base);
+                max_area = max(max_area, height[left] * (right - left));
                 left++;
             }
             else
             {
-                max_area = max(max_area, height[right] * base);
+                max_area = max(max_area, height[right] * (right - left));
                 right--;
             }
-            base--;
         }
         return max_area;
     }

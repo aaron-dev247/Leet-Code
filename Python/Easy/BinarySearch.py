5d5ffb72fccbc1f9de6c3c83a8a1319798354c80
Aaron <troublemaker.5354@gmail>
2023-10-13 04:06:56 +0800

    Binary Search solution

--- Python/Easy/BinarySearch.py
+++ Python/Easy/BinarySearch.py
@@ -1,15 +1,13 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
         left, right = 0, len(nums) - 1
 
         while left <= right:
 
-            mid = left + ((right - left) // 2)
+            mid = left + (right - left) // 2
 
             if nums[mid] == target:
                 return mid
